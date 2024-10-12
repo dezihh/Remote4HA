7c9df8a394dfb3338e8e8e106063b5d7441cc566
Derek Ziegler <derek@web.de>
2024-10-12 08:49:46 +0000

    Added Wifi and Platform settings

--- src/main.cpp
+++ src/main.cpp
@@ -18,81 +18,83 @@ AsyncWebServer server(80); // Webserver auf Port 80
 // Override HIDComposite to be able to select which interface we want to hook into
 class HIDSelector : public HIDComposite {
 public:
-    HIDSelector(USB *p) : HIDComposite(p) {};
+    HIDSelector(USB *p) : HIDComposite(p) {}
 
 protected:
-    void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf); // Called by the HIDComposite library
+    void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf);
     bool SelectInterface(uint8_t iface, uint8_t proto);
 };
 
 // Return true for the interface we want to hook into
 bool HIDSelector::SelectInterface(uint8_t iface, uint8_t proto) {
-  if (proto != 0)
-    return true;
-  return false;
+    if (proto != 0) return true;  // Hier wird die Protokoll-ID gefiltert
+    return false;
 }
 
 // Will be called for all HID data received from the USB interface
 void HIDSelector::ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf) {
-  if (len && buf)  {
-    Serial.print("HID Data: ");
-    for (uint8_t i = 0; i < len; i++) {
-      char hexChar[5];
-      sprintf(hexChar, "%02X ", buf[i]);
-      Serial.print(hexChar);
-      hidData += String(hexChar);  // Speichere die empfangenen Daten im String
+    if (len && buf) {
+        Serial.print("HID Data: ");
+        for (uint8_t i = 0; i < len; i++) {
+            char hexChar[5];
+            sprintf(hexChar, "%02X ", buf[i]);
+            Serial.print(hexChar);
+            hidData += String(hexChar);  // Speichere die empfangenen Daten im String
+        }
+        Serial.println();
     }
-    Serial.println();
-  }
 }
 
 USB Usb;
 HIDSelector hidSelector(&Usb);
 
+// Funktion zum Einrichten des Webservers
 void setupWebServer() {
-  // Starte den Webserver
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    String html = "<html><body><h1>USB HID Data</h1>";
-    html += "<p>Data: " + hidData + "</p>";
-    html += "</body></html>";
-    request->send(200, "text/html", html);  // HTML-Seite mit HID-Daten senden
-  });
-  
-  server.begin();  // Starte den Server
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        String html = "<html><body><h1>USB HID Data</h1>";
+        html += "<p>Data: " + hidData + "</p>";
+        html += "</body></html>";
+        request->send(200, "text/html", html);  // HTML-Seite mit HID-Daten senden
+    });
+
+    server.begin();  // Starte den Server
 }
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial);  // Warte auf serielle Verbindung
-  Serial.println("Start");
+    Serial.begin(115200);
+    while (!Serial);  // Warte auf serielle Verbindung
+    Serial.println("Start");
 
-  // SPI Pins definieren
-  SPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI, PIN_SS);
+    // SPI Pins definieren
+    SPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI, PIN_SS);
 
-  if (Usb.Init() == -1) {
-    Serial.println("USB Host Shield initialization failed.");
-    return;
-  }
-  Serial.println("USB Host Shield initialized.");
+    // WiFiManager initialisieren
+    WiFiManager wm;
 
-  // Set this to higher values to enable more debug information
-  UsbDEBUGlvl = 0xff;
+    // Automatisches Verbinden oder Setup-Seite anzeigen, wenn keine Verbindung hergestellt werden kann
+    if (!wm.autoConnect("ESP_USB_Host")) {
+        Serial.println("Failed to connect to WiFi");  // Diese Zeile wird nur ausgegeben, wenn die Verbindung fehlschlägt
+        // Hier kein ESP.restart(), da der ESP nun im AP-Modus ist und die Setup-Seite bereitstellt
+    }
+    Serial.println("Connected to WiFi!");
+    Serial.println(WiFi.localIP());  // Zeigt die IP-Adresse an
 
-  delay(200);
+    // Webserver einrichten
+    setupWebServer();
 
-  // WiFiManager initialisieren
-  WiFiManager wm;
-  // Automatisches Verbinden oder Setup-Seite anzeigen, wenn keine Verbindung hergestellt werden kann
-  if (!wm.autoConnect("ESP_USB_Host")) {
-    Serial.println("Failed to connect to WiFi");
-    ESP.restart();  // Neustart, wenn keine Verbindung möglich ist
-  }
-  Serial.println("Connected to WiFi!");
+    // USB-Host initialisieren
+    if (Usb.Init() == -1) {
+        Serial.println("USB Host Shield initialization failed.");
+        return;
+    }
+    Serial.println("USB Host Shield initialized.");
 
-  // Webserver einrichten
-  setupWebServer();
+    // Set this to higher values to enable more debug information
+    UsbDEBUGlvl = 0xff;
+
+    delay(200);
 }
 
 void loop() {
-  Usb.Task();  // USB-Task ausführen
+    Usb.Task();  // USB-Task ausführen
 }
